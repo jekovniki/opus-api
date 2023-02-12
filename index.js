873198b6fbe9cafc1adfc2cd502e94964996763a
jekovniki <jekovniki95@gmail.com>
2023-02-12 16:28:26 +0200

    Remove

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Github is working!");
+
+const element = document.querySelector('.input button');
+console.log("this is the element");
+element.addEventListener("click", () => {
+    console.log("Button is pressed!");
+    const input = document.querySelector(".input input");
+    console.log(input.value);
+})
