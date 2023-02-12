1d08873ab887f1f17fa050e8331ca1eb4573dc87
jekovniki <jekovniki95@gmail.com>
2023-02-12 17:59:16 +0200

    Upd

--- index-latest-v.js
+++ index-latest-v.js
@@ -1,7 +1,4 @@
-console.log("Github is working!");
-
 const element = document.querySelector('.input button');
-console.log("this is the element");
 element.addEventListener("click", async () => {
     const input = document.querySelector(".input input");
     await fetchPost(input.value);
