0b62a01095a9357e2a3de1729e741b3ae812a596
jekovniki <jekovniki95@gmail.com>
2023-02-12 20:29:24 +0200

    Final

--- index-latest-version.js
+++ index-latest-version.js
@@ -5,15 +5,13 @@ element.addEventListener("click", async () => {
 })
 
 async function fetchPost(email) {
-    console.log(1);
-    console.log("aaa");
     const request = axios.create({
         headers: {
             "Content-Type": "application/json",
             "X-Auth-Token": "api-key 3321avhagseem8fntsnnhnbs90xaaea9",
         }
     })
-    const response =  await request.post("https://cors-anywhere.herokuapp.com/http://api.getresponse.com/v3/contacts", {
+    const response =  await request.post("http://api.getresponse.com/v3/contacts", {
         "email": email,
         "campaign": {
             "campaignId": "qA9mA"
